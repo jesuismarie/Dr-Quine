4197d446aa2bbab16fa1c8e277eda52acb540b96
Marie <mari.nazaryan7173@gmail.com>
2025-12-28 00:08:43 +0400

    Colleen in asm

---
+++ ASM/Colleen/Colleen.s
@@ -0,0 +1,21 @@
+; Outside comment
+
+section .rodata
+str: db "; Outside comment%1$c%1$csection .rodata%1$cstr: db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$c    global main%1$c    extern printf%1$c%1$cmain:%1$c    push rbp%1$c    mov rbp, rsp%1$c    lea rdi, [rel str]%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    lea rcx, [rel str]%1$c    xor rax, rax%1$c    ; Inside comment%1$c    call printf%1$c    leave%1$c    ret%1$c", 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    push rbp
+    mov rbp, rsp
+    lea rdi, [rel str]
+    mov rsi, 10
+    mov rdx, 34
+    lea rcx, [rel str]
+    xor rax, rax
+    ; Inside comment
+    call printf
+    leave
+    ret
