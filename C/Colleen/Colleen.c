c1a37b5391e42676524b315756ca307b6d9a87bf
Marie <mari.nazaryan7173@gmail.com>
2026-01-03 15:12:12 +0400

    cleanup

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -6,8 +6,8 @@ void print(char *s) { printf(s, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 1
 
 int main()
 {
-    // Inside comment
-    char *s="#include <stdio.h>%c%c// Outside comment%c%cvoid print(char *s) { printf(s, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10); }%c%cint main()%c{%c    // Inside comment%c    char *s=%c%s%c;%c    print(s);%c    return 0;%c}%c";
-    print(s);
-    return 0;
+	// Inside comment
+	char *s="#include <stdio.h>%c%c// Outside comment%c%cvoid print(char *s) { printf(s, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10); }%c%cint main()%c{%c	// Inside comment%c	char *s=%c%s%c;%c	print(s);%c	return 0;%c}%c";
+	print(s);
+	return 0;
 }
