5571d8438b0b737b9491c8466f922a5fd6901e85
Marie <mari.nazaryan7173@gmail.com>
2026-03-04 13:02:19 +0400

    Bug: Fix Sully file number

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -7,20 +7,20 @@ int main() {
 	char src[64], exe[64];
 	FILE *f;
 
-	sprintf(src, "Sully/Sully_%d.c", i);
+	sprintf(src, "Sully/Sully_%d.c", i - 1);
 	f = fopen(src, "w");
 	if (!f) return 1;
 
-	char *code = "#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>%c%cint main() {%c%cint i = %d;%c%cchar src[64], exe[64];%c%cFILE *f;%c%c%csprintf(src, %cSully/Sully_%%d.c%c, i);%c%cf = fopen(src, %cw%c);%c%cif (!f) return 1;%c%c%cchar *code = %c%s%c;%c%c%cfprintf(f, code, 10, 10, 10, 10, 10, 9, i - 1, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 10, 9, 34, code, 34, 10, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 10, 10, 9, 10, 9, 9, 34, 34, 10, 9, 9, 10, 9, 10, 9, 10, 10);%c%cfclose(f);%c%c%csprintf(exe, %cgcc -o Sully/Sully_%%d Sully/Sully_%%d.c%c, i, i);%c%csystem(exe);%c%c%cif (i > 0) {%c%c%csprintf(exe, %c./Sully/Sully_%%d%c, i);%c%c%csystem(exe);%c%c}%c%creturn 0;%c}%c";
+	char *code = "#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>%c%cint main() {%c%cint i = %d;%c%cchar src[64], exe[64];%c%cFILE *f;%c%c%csprintf(src, %cSully/Sully_%%d.c%c, i - 1);%c%cf = fopen(src, %cw%c);%c%cif (!f) return 1;%c%c%cchar *code = %c%s%c;%c%c%cfprintf(f, code, 10, 10, 10, 10, 10, 9, i - 1, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 10, 9, 34, code, 34, 10, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 10, 10, 9, 10, 9, 9, 34, 34, 10, 9, 9, 10, 9, 10, 9, 10, 10);%c%cfclose(f);%c%c%csprintf(exe, %cgcc -o Sully/Sully_%%d Sully/Sully_%%d.c%c, i - 1, i - 1);%c%csystem(exe);%c%c%cif (i - 1 > 0) {%c%c%csprintf(exe, %c./Sully/Sully_%%d%c, i - 1);%c%c%csystem(exe);%c%c}%c%creturn 0;%c}%c";
 
 	fprintf(f, code, 10, 10, 10, 10, 10, 9, i - 1, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 10, 9, 34, code, 34, 10, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 10, 10, 9, 10, 9, 9, 34, 34, 10, 9, 9, 10, 9, 10, 9, 10, 10);
 	fclose(f);
 
-	sprintf(exe, "gcc -o Sully/Sully_%d Sully/Sully_%d.c", i, i);
+	sprintf(exe, "gcc -o Sully/Sully_%d Sully/Sully_%d.c", i - 1, i - 1);
 	system(exe);
 
-	if (i > 0) {
-		sprintf(exe, "./Sully/Sully_%d", i);
+	if (i - 1 > 0) {
+		sprintf(exe, "./Sully/Sully_%d", i - 1);
 		system(exe);
 	}
 	return 0;
--- Python/Sully/Sully.py
+++ Python/Sully/Sully.py
@@ -4,12 +4,12 @@ import sys
 if __name__ == "__main__":
 	i = 5
 
-	if i <= 0:
+	if i - 1 < 0:
 		sys.exit(0)
 
-	s = "import os%cimport sys%c%cif __name__ == %c__main__%c:%c	i = %d%c%c	if i <= 0:%c		sys.exit(0)%c%c	s = %c%s%c%c%c	with open(f%cSully/Sully_{i}.py%c, %cw%c) as f:%c		f.write(s %% (10, 10, 10, 34, 34, 10, i - 1, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 34, 34, 34, 34, 10, 10, 10, 34, 34, 10))%c%c	os.system(f%cpython3 Sully/Sully_{i}.py%c)%c"
+	s = "import os%cimport sys%c%cif __name__ == %c__main__%c:%c	i = %d%c%c	if i - 1 < 0:%c		sys.exit(0)%c%c	s = %c%s%c%c%c	with open(f%cSully/Sully_{i - 1}.py%c, %cw%c) as f:%c		f.write(s %% (10, 10, 10, 34, 34, 10, i - 1, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 34, 34, 34, 34, 10, 10, 10, 34, 34, 10))%c%c	os.system(f%cpython3 Sully/Sully_{i - 1}.py%c)%c"
 
-	with open(f"Sully/Sully_{i}.py", "w") as f:
+	with open(f"Sully/Sully_{i - 1}.py", "w") as f:
 		f.write(s % (10, 10, 10, 34, 34, 10, i - 1, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 34, 34, 34, 34, 10, 10, 10, 34, 34, 10))
 
-	os.system(f"python3 Sully/Sully_{i}.py")
+	os.system(f"python3 Sully/Sully_{i - 1}.py")
