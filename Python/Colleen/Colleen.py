c187226256284ab193fc81df2c25266817bfc06b
Marie <mari.nazaryan7173@gmail.com>
2026-03-05 16:12:23 +0400

    Bug: Fix difference in Colleen

--- Python/Colleen/Colleen.py
+++ Python/Colleen/Colleen.py
@@ -1,9 +1,9 @@
 # Outside comment
 
 def p(s):
-	print(s % (10, 10, 10, 10, 10, 34, 34, 10, 10, 34, s, 34, 10, 10))
+	print(s % (10, 10, 10, 10, 10, 34, 34, 10, 10, 34, s, 34, 10))
 
 if __name__ == "__main__":
 	# Inside comment
-	s = "# Outside comment%c%cdef p(s):%c	print(s %% (10, 10, 10, 10, 10, 34, 34, 10, 10, 34, s, 34, 10, 10))%c%cif __name__ == %c__main__%c:%c	# Inside comment%c	s = %c%s%c%c	p(s)%c"
+	s = "# Outside comment%c%cdef p(s):%c	print(s %% (10, 10, 10, 10, 10, 34, 34, 10, 10, 34, s, 34, 10))%c%cif __name__ == %c__main__%c:%c	# Inside comment%c	s = %c%s%c%c	p(s)"
 	p(s)
